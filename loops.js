6829c81ded5bffb0eaa3c837082927171734fd8c
Rafael Huijon <rhuijonp@gmail.com>
2018-02-09 18:36:41 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -3,7 +3,7 @@ var array = [];
 function forLoop(array){
   for (var i = 0; i < 25; i++){
     if (i === 1) {
-      array[i] = `I am ${i} strange loop`;  
+      array[i] = `I am ${i} strange loop.`;  
     } else {
       array[i] = (`I am ${i} strange loops.`)
     }
